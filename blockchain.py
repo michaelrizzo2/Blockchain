f9fcbd67cc2d03d1b01edf68191c468d70a33c5f
michaelrizzo2 <m_rizzo@live.com>
2019-12-22 18:39:55 -0500

    The third stage of the methods

--- blockchain.py
+++ blockchain.py
@@ -4,10 +4,20 @@ class Blockchain(object):
     def __init__(self):
         self.chain=[]
         self.current_transactions=[]
+        
+        #This will create the genesis transaction
+        self.new_block(previous_hash=1,proof=100)
 
-    def new_block(self):
+    def new_block(self,proof,previous_hash=None):
+        """Create a new Block in the Blockchain
+                :param proof: <int> The proof given by the Proof of Work algorithm
+                        :param previous_hash: (Optional) <str> Hash of previous Block
+                                :return: <dict> New Block
+                                        """
+
         #creates a new block and adds it to the chain
-        pass
+        block = {'index': len(self.chain) + 1, 'timestamp': time(),'transactions': self.current_transactions, 'proof': proof, 'previous_hash': previous_hash or self.hash(self.chain[-1])}
+
 
     def new_transaction(self,sender,recipient,amount):
         #adds a new transaction to the list of transactions
