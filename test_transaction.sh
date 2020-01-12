fb5df97d6f2bbf89beae9060eb4513e3fc4f8197
michaelrizzo2 <m_rizzo@live.com>
2020-01-12 18:58:39 -0500

    Added file to test new transaction

---
+++ test_transaction.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -X POST -H "Content-Type:application/json" -d '{"amount": 1, "recipient": "9e3546b87ab9433ea934d9b70d7467af", "sender": "0"}' "http://localhost:5000/transactions/new" {
+{
+  "message": "Transaction will be added to Block 2"
+}
