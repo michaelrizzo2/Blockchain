ea094deb289e172343de9e8bb1e72c3f94c61f75
michaelrizzo2 <m_rizzo@live.com>
2020-01-12 23:07:01 -0500

    got new transactions method to work fully

--- test_transaction.sh
+++ test_transaction.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-curl -X POST -H "Content-Type:application/json" -d '{"amount": 1, "recipient": "9e3546b87ab9433ea934d9b70d7467af", "sender": "0"}' "http://localhost:5000/transactions/new" {
-{
-  "message": "Transaction will be added to Block 2"
-}
+curl -X POST -H "Content-Type:application/json" -d '{"amount": 1, "recipient": "9e3546b87ab9433ea934d9b70d7467af", "sender": "0"}' "http://localhost:5000/transactions/new"
