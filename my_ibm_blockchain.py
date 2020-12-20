421505a3cd8318a829c8f4cd846b711170c053c0
michaelrizzo2 <m_rizzo@live.com>
2020-12-20 09:26:14 -0500

    fixed indentation issue

--- my_ibm_blockchain.py
+++ my_ibm_blockchain.py
@@ -4,7 +4,7 @@ import time
 import requests
 from flask import Flask,request
 class Block:
-    def __init__(self,index,transactions,timestamp,nonce,previous_hash):
+    def __init__(self,index,transactions,timestamp,previous_hash,nonce=0):
         #This will be a constructor for the block class
         self.index = index
         self.transactions = transactions
@@ -12,9 +12,49 @@ class Block:
         self.previous_hash = previous_hash
         self.nonce = nonce
     #this will compute the hash for every entry in the blockchain.
-    def compute_hash(self):
-        block_string=json.dumps(self.__dict__,sort_keys=True)
-        return sha256(block_string.encode()).hexdigest()
+        def compute_hash(self):
+            block_string=json.dumps(self.__dict__,sort_keys=True)
+            return sha256(block_string.encode()).hexdigest()
 
  #this next class will be for the actual blockchain
- class Blockchain(object):
+class Blockchain:
+    # difficulty of our PoW algorithm
+    difficulty = 2
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+
+    def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+        genesis_block = Block(0, [], 0, "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification.
+        Verification includes:
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of latest block
+          in the chain match.
+        """
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not Blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True 
