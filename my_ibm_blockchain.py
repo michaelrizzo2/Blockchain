4e41ad65395c4e0c526bb8d84ebac7724fd051e7
michaelrizzo2 <m_rizzo@live.com>
2020-12-20 09:08:22 -0500

    fixed indentatino error in block

--- my_ibm_blockchain.py
+++ my_ibm_blockchain.py
@@ -1,11 +1,20 @@
+from hashlib import sha256
+import json
+import time
+import requests
+from flask import Flask,request
 class Block:
     def __init__(self,index,transactions,timestamp,nonce,previous_hash):
         #This will be a constructor for the block class
-    """
-    :param index: Unique ID of the block.
-    :param transactions: List of transactions.
-    :param timestamp: Time of generation of the block.
-    """
-    self.index = index
-    self.transactions = transactions
-    self.timestamp = timestamp
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+    #this will compute the hash for every entry in the blockchain.
+    def compute_hash(self):
+        block_string=json.dumps(self.__dict__,sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+ #this next class will be for the actual blockchain
+ class Blockchain(object):
